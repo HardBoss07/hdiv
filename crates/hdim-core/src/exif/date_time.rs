0f1f494207758ff38e97b5501900f320bb78c85a
HardBoss07 <matteo@bosshard.net>
2026-01-30 15:17:05 +0100

    feat(exif): implemented own get_date_time_data() in date_time.rs

--- crates/hdim-core/src/exif/date_time.rs
+++ crates/hdim-core/src/exif/date_time.rs
@@ -1,6 +1,17 @@
+use super::util::get_ascii;
+use exif::{Exif, In, Tag};
+
 #[derive(Clone, Debug)]
 pub struct DateTimeExif {
     pub original: Option<String>,
     pub digitized: Option<String>,
     pub modified: Option<String>,
+}
+
+pub fn get_date_time_exif(exif: &Exif) -> Option<DateTimeExif> {
+    Some(DateTimeExif {
+        original: get_ascii(exif.get_field(Tag::DateTimeOriginal, In::PRIMARY)),
+        digitized: get_ascii(exif.get_field(Tag::DateTimeDigitized, In::PRIMARY)),
+        modified: get_ascii(exif.get_field(Tag::DateTime, In::PRIMARY)),
+    })
 }
