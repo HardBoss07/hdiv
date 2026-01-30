b9c97f172695d76f1668da5eb495e90d5333b4a5
HardBoss07 <matteo@bosshard.net>
2026-01-30 15:19:27 +0100

    feat(exif): implemented own get_lens_data() in lens.rs

--- crates/hdim-core/src/exif/lens.rs
+++ crates/hdim-core/src/exif/lens.rs
@@ -1,3 +1,6 @@
+use super::util::{get_ascii, get_rational_vec};
+use exif::{Exif, In, Tag};
+
 #[derive(Clone, Debug)]
 pub struct LensExif {
     pub make: Option<String>,
@@ -6,4 +9,16 @@ pub struct LensExif {
     pub max_focal_length: Option<f64>,
     pub min_aperture: Option<f64>,
     pub max_aperture: Option<f64>,
+}
+
+pub fn get_lens_exif(exif: &Exif) -> Option<LensExif> {
+    let lens_spec = exif.get_field(Tag::LensSpecification, In::PRIMARY);
+    Some(LensExif {
+        make: get_ascii(exif.get_field(Tag::LensMake, In::PRIMARY)),
+        model: get_ascii(exif.get_field(Tag::LensModel, In::PRIMARY)),
+        min_focal_length: get_rational_vec(lens_spec).and_then(|v| v.get(0).copied()),
+        max_focal_length: get_rational_vec(lens_spec).and_then(|v| v.get(1).copied()),
+        min_aperture: get_rational_vec(lens_spec).and_then(|v| v.get(2).copied()),
+        max_aperture: get_rational_vec(lens_spec).and_then(|v| v.get(3).copied()),
+    })
 }
