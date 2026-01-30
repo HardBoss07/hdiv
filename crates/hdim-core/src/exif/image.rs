3f7af8ae4933649af9172aec3918c64c0451d21f
HardBoss07 <matteo@bosshard.net>
2026-01-30 15:18:49 +0100

    feat(exif): implemented own get_image_data() in image.rs

--- crates/hdim-core/src/exif/image.rs
+++ crates/hdim-core/src/exif/image.rs
@@ -1,6 +1,27 @@
+use super::util::get_rational;
+use exif::{Exif, In, Tag};
+
 #[derive(Clone, Debug)]
 pub struct ImageExif {
     pub width: Option<u32>,
     pub height: Option<u32>,
-    pub color_space: Option<u16>,
+    pub resolution_unit: Option<u16>,
+    pub x_resolution: Option<f64>,
+    pub y_resolution: Option<f64>,
+}
+
+pub fn get_image_exif(exif: &Exif) -> Option<ImageExif> {
+    Some(ImageExif {
+        width: exif
+            .get_field(Tag::ImageWidth, In::PRIMARY)
+            .and_then(|f| f.value.get_uint(0)),
+        height: exif
+            .get_field(Tag::ImageLength, In::PRIMARY)
+            .and_then(|f| f.value.get_uint(0)),
+        resolution_unit: exif
+            .get_field(Tag::ResolutionUnit, In::PRIMARY)
+            .and_then(|f| f.value.get_uint(0).map(|v| v as u16)),
+        x_resolution: get_rational(exif.get_field(Tag::XResolution, In::PRIMARY)),
+        y_resolution: get_rational(exif.get_field(Tag::YResolution, In::PRIMARY)),
+    })
 }
