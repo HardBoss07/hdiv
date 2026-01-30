d5b5ea41c9cf40b341bbbb51d7fbc7fe6a340166
HardBoss07 <matteo@bosshard.net>
2026-01-30 15:16:32 +0100

    feat(exif): implemented own get_camera_data() in camera.rs

--- crates/hdim-core/src/exif/camera.rs
+++ crates/hdim-core/src/exif/camera.rs
@@ -1,6 +1,17 @@
+use super::util::get_ascii;
+use exif::{Exif, In, Tag};
+
 #[derive(Clone, Debug)]
 pub struct CameraExif {
     pub make: Option<String>,
     pub model: Option<String>,
     pub software: Option<String>,
+}
+
+pub fn get_camera_exif(exif: &Exif) -> Option<CameraExif> {
+    Some(CameraExif {
+        make: get_ascii(exif.get_field(Tag::Make, In::PRIMARY)),
+        model: get_ascii(exif.get_field(Tag::Model, In::PRIMARY)),
+        software: get_ascii(exif.get_field(Tag::Software, In::PRIMARY)),
+    })
 }
